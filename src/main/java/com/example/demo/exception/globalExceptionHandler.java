acb045df4b65499ab6191d7915429baebb5ac33f
bala-amypo <bala@amypo.in>
2025-12-18 06:33:18 +0000

    User commit

--- src/main/java/com/example/demo/exception/globalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/globalExceptionHandler.java
@@ -1,23 +1,23 @@
 package com.example.demo.exception;
 
-import java.util.*;
-
-import org.springframework.http.*;//response entity
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import java.util.*;
+
 
 @RestControllerAdvice
+public class globalExceptionHandler{
+     
+     @ExceptionHandler(MethodArgumentNotValidException.class)
+     public ResponseEntity<?>handleFieldError(MethodArgumentNotValidException ex){
+        Map<String,String>error=new HashMap<>();
 
-public class globalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleFieldEroor(MethodArgumentNotValidException ex){
-        Map<String,String> error = new HashMap<>();
-
-        ex.getBindingResult().getFieldError().forEach(err -> error.put(err.getField(),err.getDefaultMessage()));
+        ex.getBindingResult().getFieldErrors().forEach( err -> error.put(err.getField(),err.getDefaultMessage()));
 
         return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
-    }
 
-
+ 
+     }
 }
