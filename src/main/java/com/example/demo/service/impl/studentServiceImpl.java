6fbb0e005f9e6a8f32809c012de4f673b7fa4162
bala-amypo <bala@amypo.in>
2025-12-18 08:13:18 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/studentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/studentServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.*;
 import com.example.demo.entity.*;
 import com.example.demo.repository.*;
 import com.example.demo.service.*;
+import com.example.demo.exception.*;
 
 @Service
 public class studentServiceImpl implements studentService{
@@ -22,4 +23,7 @@ public class studentServiceImpl implements studentService{
         return repo.save(student);
     }
 
+    public studentEntity getById(Long id){
+        return repo.findById(id).orElseThrow(() -> new StudentNotFoundException("Student ID not Found"));
+    }
 }
